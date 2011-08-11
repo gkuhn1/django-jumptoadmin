7e9bab0901f4a9898a21e94d039ec4ea4d98ac84
Guilherme Kuhn <g.kuhn0@gmail.com>
2011-08-11 11:34:05 -0300

    Turn the code of context_processors.py more readable

--- jumptoadmin/context_processors.py
+++ jumptoadmin/context_processors.py
@@ -5,24 +5,16 @@ def media(request):
     Returns the URL of jumptoadmin media specified in settings
     Defaults to the standard MEDIA_URL setting + '/jumptoadmin/'
     """
-    
-    try:
-        JUMPTOADMIN_MEDIA_URL = settings.JUMPTOADMIN_MEDIA_URL
-    
-    except AttributeError:
-        JUMPTOADMIN_MEDIA_URL = ''
-        
+    JUMPTOADMIN_MEDIA_URL = getattr(settings, 'JUMPTOADMIN_MEDIA_URL', '')
+
+    if not JUMPTOADMIN_MEDIA_URL:
         # Only need to get MEDIA_URL if JUMPTOADMIN_MEDIA_URL is not specified
-        try:
-            MEDIA_URL = settings.MEDIA_URL
-            
-            # Make sure MEDIA_URL ends in a '/'
-            if MEDIA_URL[-1] != '/':
-                MEDIA_URL += '/'
-                
-            JUMPTOADMIN_MEDIA_URL = '%sjumptoadmin/' % (MEDIA_URL)
-            
-        except AttributeError:
-            MEDIA_URL = ''
-            
+        MEDIA_URL = getattr(settings, 'MEDIA_URL', '')
+
+        # Make sure MEDIA_URL ends in a '/'
+        if MEDIA_URL[-1] != '/':
+            MEDIA_URL += '/'
+
+        JUMPTOADMIN_MEDIA_URL = '%sjumptoadmin/' % (MEDIA_URL)
+
     return {'JUMPTOADMIN_MEDIA_URL': JUMPTOADMIN_MEDIA_URL}
